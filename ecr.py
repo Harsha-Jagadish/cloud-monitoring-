0db116e63213f48128a6065343aa4023e9135c13
Harsha-Jagadish <harshajagadish99@gmail.com>
2024-02-27 18:23:16 -0800

    Included working screenshot

--- ecr.py
+++ ecr.py
@@ -1,9 +1,26 @@
 import boto3
 
-ecr_client = boto3.client('ecr')
+# Create a repository using the ECR API calls from the AWS SDK BOTO for python
+#Documentation: https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/ecr/client/create_repository.html
+
+# Create client ecr 
+ecr_client=boto3.client('ecr', region_name='us-east-1')
 
 repository_name = "my_monitoring_app_image"
 response = ecr_client.create_repository(repositoryName=repository_name)
 
+# lets get the repo uri which we will use when setting up the eks client 
 repository_uri = response ['repository']['repositoryUri']
 print(repository_uri)
+
+# Once we have the ecr repository uri go over the aws console to check if it has been created to verify 
+# Now we need to upload our docker image to the repository
+# Follow the push commands provided in the ecr repository
+# make sure to add the required permissions/policies to the IAM user to allow login access and uploading the image
+# Comeplete list of permissions in JSON File
+
+# Once we have pushed the image to the repository
+# We create a EKS cluster on the AWS console and connect to a default VPC and subnets (create a EKS role with permissions)
+# We need to ensure that the security group configuration is avaible for the port we are using in our application image (5000)
+# Create EKS cluster and add node group but first create a EC2 role for your node group to allow access for container registry
+# Once node group is created lets move on to the Kubernetes Service 
--- eks.py
+++ eks.py
@@ -1,13 +1,25 @@
+# Documentation: https://kubernetes.io/docs/concepts/workloads/controllers/deployment/
+# Instead of yaml lets use python to define the deployment configuration
+
 #create deployment and service
 from kubernetes import client, config
 
-# Load Kubernetes configuration
-config.load_kube_config()
+# Add the permissions for the user to eks:describeCluster to allow the client to load the kube config 
+# Path to your kubeconfig file
+kubeconfig_path = '/Users/harsha/.kube/config'
+
+
+# Load Kubernetes configuration that we have created on eks using the AWS console and 
+config.load_kube_config(config_file=kubeconfig_path)
 
 # Create a Kubernetes API client
 api_client = client.ApiClient()
 
-# Define the deployment
+# Define the deployment 
+# A Deployment provides declarative updates for Pods and ReplicaSets.
+# You describe a desired state in a Deployment, and the Deployment Controller changes the actual state to the desired state at a controlled rate. 
+# You can define Deployments to create new ReplicaSets, or to remove existing Deployments and adopt all their resources with new Deployments.
+
 deployment = client.V1Deployment(
     metadata=client.V1ObjectMeta(name="my-flask-app"),
     spec=client.V1DeploymentSpec(
@@ -23,7 +35,7 @@ deployment = client.V1Deployment(
                 containers=[
                     client.V1Container(
                         name="my-flask-container",
-                        image="568373317874.dkr.ecr.us-east-1.amazonaws.com/my_monitoring_app_image:latest",
+                        image="851725605398.dkr.ecr.us-east-1.amazonaws.com/my_monitoring_app_image",
                         ports=[client.V1ContainerPort(container_port=5000)]
                     )
                 ]
@@ -32,14 +44,17 @@ deployment = client.V1Deployment(
     )
 )
 
-# Create the deployment
+# Create the deployment using python code instead of doing it in cli using "kubectl apply -f deployement.yaml default"
 api_instance = client.AppsV1Api(api_client)
 api_instance.create_namespaced_deployment(
     namespace="default",
     body=deployment
 )
 
-# Define the service
+# Define the service so everyone else outside the cluster can access it
+# Documentation: https://kubernetes.io/docs/concepts/services-networking/service/
+# No need to implememt load balancer I would love to work on large scale projects that do utilize this to learn more
+
 service = client.V1Service(
     metadata=client.V1ObjectMeta(name="my-flask-service"),
     spec=client.V1ServiceSpec(
@@ -54,3 +69,9 @@ api_instance.create_namespaced_service(
     namespace="default",
     body=service
 )
+
+# Once you run this script you can montior the kube pods, deployment and service using the following commands
+# Pods: kubectl get pods -n default -w
+# Deployment: kubectl get deployment -n default 
+# Service: kubectl get svc -n default
+# More details about pods: kubectl describe pods ${p0d_name} -n default
